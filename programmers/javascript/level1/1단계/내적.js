6a5337e0aab250cdf4c84d5890115bd12c476b3e
정성운 <ggogumalove@gmail.com>
2020-11-24 00:43:40 +0900

    11월 24일

---
+++ programmers/javascript/level1/1단계/내적.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++){
+        sum += a[i] * b[i]
+    }
+    return sum;
+}
+
+function solution(a, b) {
+    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+}
